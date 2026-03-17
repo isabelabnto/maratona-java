f94dd5ec34da087feebe8c1a808a1537eacf4182
Isabela Bento <isabelaebento@gmail.com>
2026-03-16 22:57:39 -0300

    Referência this e teste com metodo de impressão

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -5,5 +5,11 @@ public class Estudante {
     public int idade;
     public char sexo;
 
-
+    public void imprime(){
+        // this se refere ao atributo dentro do objeto/classe
+        System.out.println("--------------------");
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+    }
 }
